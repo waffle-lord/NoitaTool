b10cc0d360182cc47ff1d2ff7ece13c5c24f4cd1
waffle.lord <waffle.lord@hotmail.com>
2024-05-15 15:08:06 -0400

    add initial wails project

---
+++ notiatool/frontend/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<main style="display:flex; height:100%;">
+    <div class="center" style="flex:1">
+      <h1>Hello There</h1>
+    </div>
+</main>
+
+
+<style>
+	.center {
+		text-align: center;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+	}
+</style>
